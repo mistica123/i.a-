d4e26f1bffacf40ae0736c20c5e9b84e7990222a
mistica123 <168596151+mistica123@users.noreply.github.com>
2024-08-29 18:38:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,116 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual é o seu gênero de filme favorito?",
+        alternativas: [
+            {
+                texto: "A) Ação",
+                afirmacao: [
+                    "Você é como James Bond. Destemido, carismático e sempre preparado para a próxima missão. Sua vida é cheia de aventuras e desafios, e você enfrenta cada um com confiança e estilo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Comédia",
+                afirmacao: [
+                    "Você é como Deadpool. Divertido, irreverente e com uma resposta afiada para tudo. Você sabe como transformar qualquer situação em uma piada e manter o clima leve, mesmo nos momentos mais tensos."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Como você reage em situações de perigo?",
+        alternativas: [
+            {
+                texto: "Enfrento de frente, sem medo",
+                afirmacao: [
+                    "Você é como Indiana Jones. Corajoso e destemido, você não hesita em se jogar em situações perigosas para alcançar seus objetivos. Sua vida é uma série de aventuras épicas, sempre em busca de novos tesouros e descobertas."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Procuro uma solução criativa",
+                afirmacao: [
+                    "Você é como Sherlock Holmes. Inteligente e astuto, você prefere usar sua mente para sair de situações complicadas. Seu raciocínio lógico e habilidades dedutivas fazem de você um mestre em resolver problemas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "3. Qual é a sua qualidade mais marcante?",
+        alternativas: [
+            {
+                texto: "Lealdade",
+                afirmacao: [
+                    "Você é como Samwise Gamgee, de 'O Senhor dos Anéis'. Sua lealdade aos amigos é inabalável, e você está sempre pronto para apoiar aqueles que ama, não importa o quão difícil seja o caminho."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Independência",
+                afirmacao: [
+                    "Você é como Katniss Everdeen, de 'Jogos Vorazes'. Forte e independente, você confia em suas habilidades e não precisa de ninguém para protegê-lo. Você luta por aquilo em que acredita e não tem medo de enfrentar o sistema."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "4. Como você gosta de passar o tempo livre?",
+        alternativas: [
+            {
+                texto: "Explorando novos lugares",
+                afirmacao: [
+                    "Você é como Lara Croft, de 'Tomb Raider'. Aventura é o seu nome do meio. Você adora explorar, descobrir segredos escondidos e desafiar seus próprios limites. Seu espírito é livre e curioso."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Relaxando em casa",
+                afirmacao: [
+                    "Você é como Bilbo Baggins, de 'O Hobbit'. Você valoriza o conforto do lar e gosta de passar seu tempo em um ambiente tranquilo, apreciando as pequenas coisas da vida. Mas, mesmo assim, não foge de uma aventura inesperada."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "5. Qual é o seu maior objetivo na vida?",
+        alternativas: [
+            {
+                texto: "Encontrar o amor verdadeiro",
+                afirmacao: [
+                    "Você é como Bridget Jones, de 'O Diário de Bridget Jones'. Você acredita no amor e está disposto a enfrentar todas as dificuldades para encontrá-lo. Mesmo com tropeços, você continua otimista e cheio de esperança."
+                ],
+                proxima: 9,
+            },
+            {
+                texto: "Deixar um legado",
+                afirmacao: [
+                    "Você é como Tony Stark, de 'Homem de Ferro'. Ambicioso e visionário, você quer fazer algo que faça a diferença no mundo e deixe uma marca duradoura. Seu carisma e inteligência fazem de você um líder nato."
+                ],
+                proxima: 10,
+            },
+        ]
+    },
+    {
+        enunciado: "6. Como você lida com os seus inimigos?",
+        alternativas: [
+            {
+                texto: "Com estratégia e calma",
+                afirmacao: [
+                    "Você é como Michael Corleone, de 'O Poderoso Chefão'. Frio e calculista, você prefere usar sua inteligência e estratégias para lidar com os inimigos. Você sabe como jogar o jogo e sempre está um passo à frente."
+                ],
+                proxima: 11,
+            },
+            {
+                texto: "Com força e determinação",
+                afirmacao: [
+                    "Você é como Sarah Connor, de 'O Exterminador do Futuro'. Você não se intimida facilmente e está disposto a lutar com todas as suas forças para proteger o que é importante para você. Sua determinação é inquebrável."
+                ],
+                proxima: 12,
+            },
+        ]
+    },
+];
